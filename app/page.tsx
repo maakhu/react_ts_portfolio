65b65b789d2628fc73d7386185b4adda069e9555
maakhu <egri@me.com>
2023-05-09 22:20:13 +0200

    add design changes for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,7 @@ import { useState } from "react";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
+  const lightText = "text-gray-700";
   const darkText = "dark:text-slate-300";
   return (
     <div className={darkMode ? "dark" : ""}>
@@ -27,16 +28,16 @@ export default function Home() {
       <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-800">
         {" "}
         {/* padding for  medium and large screens*/}
-        <section className="min-h-screen">
+        <section className="max-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
             {" "}
             {/* padding top & bottom 10 */}
-            <h1 className={`text-xl font-extralight ${darkText}`}>developedbymaak</h1>
+            <h1 className={`text-xl font-extralight ${lightText} ${darkText}`}>developedbymaak</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill
                   onClick={() => setDarkMode(!darkMode)}
-                  className={`cursor-pointer text-2xl text-black ${darkText}`}
+                  className={`cursor-pointer text-2xl ${lightText} ${darkText}`}
                 />
               </li>
               <li>
@@ -53,28 +54,28 @@ export default function Home() {
             <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
               Hi, I'm Máté Egri
             </h2>
-            <h3 className="text-2xl py-2 md:text-3xl dark:text-slate-300">
+            <h3 className={`text-2xl py-2 md:text-3xl ${darkText}`}>
               Full-stack Developer and Designer
             </h3>
             <p className="text-md py-5 leading-8 text-gray-700 md:text-xl max-w-xl mx-auto dark:text-slate-500">
               Freelancer Web-development Magician
             </p>
           </div>
-          <div className="text-5xl flex justify-center gap 16 py-3 text-gray-600 dark:text-slate-300">
+          <div className="text-5xl flex justify-center gap-6 py-3 text-gray-600 dark:text-slate-300">
             <AiFillLinkedin />
             <AiFillGithub />
           </div>
-          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-60 h-60 mt-20 overflow-hidden md:h-96 md:w-96 sm: h-40 w-40 mt-4">
             <Image
               src={mateDev}
-              className="rounded-full"
+              className="rounded-full "
               alt={"Cartonised profile picture"}
             />
           </div>
         </section>
         <section>
           <div>
-            <h3 className="text-3xl py1 md:text-3xl max-w-xl mx-auto dark:text-slate-200">
+            <h3 className={`text-3xl py1 md:text-3xl max-w-xl mx-auto pt-10 pb-5 ${darkText}`}>
               Services I offer
             </h3>
             <p className="text-md py-2 leading-8 text-gray-700 md:text-xl max-w-xl mx-auto dark:text-slate-300">
@@ -101,7 +102,7 @@ export default function Home() {
           </div>
           <div className="lg:flex gap-10">
             <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
-              <Image src={design} width={100} height={100} />
+              <Image src={design} width={100} height={100} style={{ margin: "0 auto" }} alt="design logo"/>
               <h3 className="text-lg font-medium pt-8 pb-2  ">
                 Beautiful Designs
               </h3>
@@ -111,12 +112,12 @@ export default function Home() {
               </p>
               <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
               <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+              <p className="text-gray-800 py-1">Affinity Photo</p>
+              <p className="text-gray-800 py-1">CSS</p>
+              <p className="text-gray-800 py-1">TailWind</p>
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={code} width={100} height={100} />
+              <Image src={code} width={100} height={100} style={{ margin: "0 auto" }} alt="code logo"/>
               <h3 className="text-lg font-medium pt-8 pb-2 ">
                 Code your dream project
               </h3>
@@ -124,35 +125,32 @@ export default function Home() {
                 Do you have an idea for your next great website? Let's make it a
                 reality.
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+              <h4 className="py-4 text-teal-600">Latest Technologies</h4>
+              <p className="text-gray-800 py-1">TypeScript</p>
+              <p className="text-gray-800 py-1">React.JS</p>
+              <p className="text-gray-800 py-1">Node.JS & Express</p>
+              <p className="text-gray-800 py-1">MongoDB</p>
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={consulting} width={100} height={100} />
+              <Image src={consulting} width={100} height={100} style={{ margin: "0 auto" }} className="text-right" alt="like logo"/>
               <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
               <p className="py-2">
-                Are you interested in feedback for your current project? I can
-                give you tips and tricks to level it up.
+                Are you interested in feedback for your current project? Here it is how can I help you.
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+              <h4 className="py-4 text-teal-600">I can assist you with</h4>
+              <p className="text-gray-800 py-1">Code review</p>
+              <p className="text-gray-800 py-1">Collaborate</p>
+              <p className="text-gray-800 py-1">Design review</p>
+              <p className="text-gray-800 py-1">Deploying</p>
             </div>
           </div>
         </section>
         <section>
           <div>
-            <h3 className="text-3xl py1">Portfolio</h3>
-            <p className="text-md py-2 leading-8 text-gray-700">
-              As a full stack web developer, I can help you build and maintain
-              both the front-end and the back-end of your website or
-              application. I have expertise in various technologies and
-              frameworks, such as
+            <h3 className={`text-3xl py1 md:text-xl max-w-xl mx-auto ${darkText}`}>Portfolio</h3>
+            <p className={`text-md py-2 leading-8 md:text-xl max-w-xl mx-auto text-gray-700 ${darkText}`}>
+              I have worked on a full-stack browser-based online multiplayer game project called Tribes,
+              where a player can  
               <span className="text-teal-500">
                 HTML, CSS, JavaScript, React, Angular, Node.js, MongoDB, MySQL,
                 Laravel
