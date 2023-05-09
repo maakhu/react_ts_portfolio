24f964b6a18c0e2dd37872b10299d8df3f25d077
maakhu <egri@me.com>
2023-05-09 17:42:59 +0200

    add new pictures and add new section for services

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import { BsFillMoonStarsFill } from "react-icons/bs";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import Image from "next/image";
+import mateDev from "../public/mateAvatar.png"; // Import using public folder
 
 export default function Home() {
   return (
@@ -10,17 +13,63 @@ export default function Home() {
       </Head>
       <main className="bg-white px-10">
         <section className="min-h-screen">
-          <nav className="py-10 mb-12 flex justify-between">  {/* padding top & bottom 10 */}
-            <h1 className="text-xl">developedbyMt</h1>
+          <nav className="py-10 mb-12 flex justify-between">
+            {" "}
+            {/* padding top & bottom 10 */}
+            <h1 className="text-xl font-extralight">developedbymaak</h1>
             <ul className="flex items-center">
               <li>
-                <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
               </li>
               <li>
-                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
               </li>
             </ul>
           </nav>
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium">
+              Hi, I'm Máté Egri
+            </h2>
+            <h3 className="text-2xl py-2">Full-stack Developer and Designer</h3>
+            <p className="text-md py-5 leading-8 text-gray-700">
+              Freelancer and other details
+            </p>
+          </div>
+          <div className="text-5xl flex justify-center gap 16 py-3 text-gray-600">
+            <AiFillLinkedin />
+            <AiFillGithub />
+          </div>
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+            <Image
+              src={mateDev}
+              className="rounded-full"
+              alt={"Cartonised profile picture"}
+            />
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className="text-3xl py1">Services I offer</h3>
+            <p>
+              As a full stack web developer, I can help you build and maintain
+              both the front-end and the back-end of your website or
+              application. I have expertise in various technologies and
+              frameworks, such as HTML, CSS, JavaScript, React, Angular,
+              Node.js, MongoDB, MySQL, Laravel, and more. I can design and
+              create user-friendly interfaces, web services and APIs, data
+              storage and processing solutions, and other features that meet
+              your business needs. I can also perform tests, troubleshoot
+              software, fix bugs, and collaborate with other developers on
+              projects. Whether you need a simple website or a complex web
+              application, I can deliver high-quality and scalable full stack
+              development solutions for you.
+            </p>
+          </div>
         </section>
       </main>
     </div>
